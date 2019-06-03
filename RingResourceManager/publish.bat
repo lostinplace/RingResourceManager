81fab9258814d9c8d6b667406c1bc100bc48810b
Chris Wheeler <cmwhee@gmail.com>
2019-06-02 20:04:28 -0400

    updated test adapters

--- RingResourceManager/publish.bat
+++ RingResourceManager/publish.bat
@@ -1,1 +1,2 @@
-﻿nuget.exe push RingResourceManager.1.0.0.nupkg -Source https://api.nuget.org/v3/index.json
+﻿nuget.exe push RingResourceManager.0.0.1.nupkg -Source https://api.nuget.org/v3/index.json
+nuget.exe push RingResourceManager.0.0.1.nupkg -Source github
